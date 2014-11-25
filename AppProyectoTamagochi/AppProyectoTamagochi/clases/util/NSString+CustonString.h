026d0c4f5d0641352bbdad486e8c611093686e13
Analia Rossi <analia.rossi@globant.com>
2014-11-25 17:50:19 -0300

    Dia7_Finalizado

---
+++ AppProyectoTamagochi/AppProyectoTamagochi/clases/util/NSString+CustonString.h
@@ -0,0 +1,17 @@
+//
+//  NSString+CustonString.h
+//  AppProyectoTamagochi
+//
+//  Created by Analia on 25/11/14.
+//  Copyright (c) 2014 Analia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString(CustonString)
+
+-(BOOL)EsVacio;
+-(BOOL)Sololetras;
+-(BOOL)MayorCuatroLetras;
+
+@end
---
+++ AppProyectoTamagochi/AppProyectoTamagochi/clases/util/NSString+CustonString.m
@@ -0,0 +1,48 @@
+//
+//  NSString+CustonString.m
+//  AppProyectoTamagochi
+//
+//  Created by Analia on 25/11/14.
+//  Copyright (c) 2014 Analia. All rights reserved.
+//
+
+#import "NSString+CustonString.h"
+
+@implementation NSString(CustonString)
+
+-(BOOL)EsVacio
+{
+    BOOL respuesta ;
+    if ([self isEqualToString:@""])
+    {
+         BOOL respuesta = YES;
+        //validamos si esta vacio
+     }
+    else
+    {
+        respuesta= NO;
+    }
+    return respuesta;
+}
+-(BOOL)MayorCuatroLetras
+{
+    BOOL respuesta;
+    if (self.length < 4)
+    {
+        respuesta=YES;
+    }
+    else{respuesta=NO;}
+
+    return respuesta;
+}
+-(BOOL)Sololetras
+{
+    BOOL respuesta;
+    if ([self rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"0123456789-,.:;}{[]^`´+*¨¡?=))(&%$#!><_"]].location != NSNotFound) {
+        respuesta=YES;
+    }
+    else
+    {respuesta= NO;}
+    return respuesta;
+}
+@end
